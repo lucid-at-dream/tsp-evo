a795ec45151b5a66f904f6128c894307f71b6b78
Bernardo Marques <bernardomarques5@gmail.com>
2019-08-03 15:57:21 +0100

    Borrow a thread pool implementation from citylife

---
+++ queue.h
@@ -0,0 +1,21 @@
+#pragma once
+
+typedef struct _queue_item {
+    void *content;
+    volatile struct _queue_item* prev;
+    volatile struct _queue_item* next;
+} queue_item;
+
+typedef struct _queue {
+    volatile int size;
+    volatile queue_item *head;
+    volatile queue_item *last;
+} queue;
+
+// Create and delete a queue
+void queue_del(queue *q);
+queue *queue_new();
+
+// Add/Pop to/from queue
+void queue_add(queue *, void *);
+void *queue_pop(queue *q);
---
+++ worker_pool.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "queue.h"
+
+#include <pthread.h>
+
+typedef struct _worker_pool {
+    int num_threads;
+    void (*do_work)(void *);
+    pthread_t *threads;
+    queue *work_queue;
+    volatile char stop;
+
+    pthread_mutex_t queue_mutex;
+    pthread_cond_t  await_work_cond;
+    pthread_cond_t  await_finish_cond;
+} worker_pool;
+
+worker_pool *pool_new(int num_threads, void (*)(void *));
+void pool_del(worker_pool *pool);
+
+void pool_start(worker_pool *pool);
+void pool_stop(worker_pool *pool);
+
+void pool_add_work(worker_pool *pool, void *job);
+void pool_await_empty_queue(worker_pool *pool);
