32ea6c815f92b2807aa77a84538b83cebc2408c7
Bernardo Marques <bernardomarques5@gmail.com>
2019-08-03 10:36:26 +0100

    Adds recombination via sex/crossover

--- refactor.c
+++ refactor.c
@@ -11,10 +11,15 @@
 double costs[MAXN][MAXN];
 
 // Data structures
-typedef struct _individual{
+typedef struct _individual {
     int *perm;
     double fitness;
 } individual;
+
+typedef struct _couple {
+    individual *a;
+    individual *b;
+} couple;
 
 individual best;
 
@@ -24,22 +29,28 @@ void magic(
         int npops, int popsize, int indsize, int ngens,
         int maxgrad0count,
         double swap_mutation_rate, double inversion_mutation_rate,
-        int elitesize
+        int elitesize,
+        int tournament_size, double crossover_rate
 );
 
+individual **initializePopulations(int npops, int popsize, int indsize);
 individual *newIndividual(individual *new, int indsize);
 void evalPopFitness(individual *population, int popsize, int indsize);
 double evalIndFitness(individual *ind, int indsize);
 
 void evolvePopulation(
-    individual *population, int popsize, int indsize, 
+    individual *population, individual *nextpop, int popsize, int indsize, 
     double swap_mutation_rate, double inversion_mutation_rate, 
-    int elitesize
+    int elitesize,
+    int tournament_size, double crossover_rate
 );
 
+couple tournament(int tsize, individual *population, int popsize);
+void crossOver(couple parents, individual *maria, individual *zezinho, int indsize);
 void swapMutation(individual *ind, int indsize);
 void subsequenceInversionMutation(individual *ind, int indsize);
 int fit_cmp(const void *ls, const void *rs);
+int fit_cmp_ptr(const void *ls, const void *rs);
 
 void printIndividual(individual *ind, int indsize);
 
@@ -58,8 +69,8 @@ int main(){
     // ===== Parameterization =====
 
     // How much machinery for heavy lifting?
-    int popsize = 2500;
-    int npops = 100;
+    int popsize = 1000;
+    int npops = 50;
     int ngens = 2000;
 
     // How much iterations without improvements before throwing the towel?
@@ -70,12 +81,16 @@ int main(){
     double inversion_mutation_rate = 0.3;
 
     // How much not randomness?
-    int elitesize = 50;
+    int elitesize = 3;
+    int tournament_size = 5;
+
+    // How much recombination of the individuals?
+    double crossover_rate = 0.7;
 
     // ===== Parameterization =====
 
     // call tsp evo
-    magic(npops, popsize, N, ngens, maxgrad0count, swap_mutation_rate, inversion_mutation_rate, elitesize);
+    magic(npops, popsize, N, ngens, maxgrad0count, swap_mutation_rate, inversion_mutation_rate, elitesize, tournament_size, crossover_rate);
 
     return 0;
 }
@@ -87,7 +102,8 @@ void magic(
         int npops, int popsize, int indsize, int ngens,
         int maxgrad0count,
         double swap_mutation_rate, double inversion_mutation_rate,
-        int elitesize
+        int elitesize,
+        int tournament_size, double crossover_rate
     ) {
 
     // Keep a reference of the best individual ever.
@@ -101,15 +117,9 @@ void magic(
     start = clock();
 
     //generate initial populations
-    individual **populations = (individual **)malloc(npops * sizeof(individual *));
-    for (int p = 0; p < npops; p++) {
-        individual *population = (individual *)malloc(popsize * sizeof(individual));
-        for (int i = 0; i < popsize; i++) {
-            newIndividual(population + i, indsize);
-        }
-        evalPopFitness(population, popsize, indsize);
-        populations[p] = population;
-    }
+    individual **populations = initializePopulations(npops, popsize, indsize);
+    individual **nextpops = initializePopulations(npops, popsize, indsize);
+
     best = populations[0][0];
 
     // generations loop
@@ -135,8 +145,19 @@ void magic(
 
         // Evolve the populations
         for (int p = 0; p < npops; p++) {
-            evolvePopulation(populations[p], popsize, indsize, swap_mutation_rate, inversion_mutation_rate, elitesize);
+            evolvePopulation(
+                populations[p], nextpops[p],
+                popsize, indsize,
+                swap_mutation_rate, inversion_mutation_rate,
+                elitesize,
+                tournament_size, crossover_rate
+            );
         }
+
+        // Swap nextpops with populations for double buffering (i.e. avoid memory allocation overhead).
+        individual **tmp = populations;
+        populations = nextpops;
+        nextpops = tmp;
 
 #ifdef V
         printf("[%04d] Best: %lf\n", gen, best.fitness);
@@ -160,6 +181,26 @@ void magic(
     // Time statistics
     finish = clock();
     printf("Took %lf seconds\n", (double)((finish - start)/(double)CLOCKS_PER_SEC) );
+}
+
+/**
+ * Allocates memory for npops * popsize individuals initialized randomly.
+ */
+individual **initializePopulations(int npops, int popsize, int indsize) {
+    individual **populations = (individual **)malloc(npops * sizeof(individual *));
+
+    for (int p = 0; p < npops; p++) {
+        individual *population = (individual *)malloc(popsize * sizeof(individual));
+
+        for (int i = 0; i < popsize; i++) {
+            newIndividual(&(population[i]), indsize);
+        }
+
+        evalPopFitness(population, popsize, indsize);
+        populations[p] = population;
+    }
+
+    return populations;
 }
 
 /**
@@ -227,27 +268,153 @@ double evalIndFitness(individual *ind, int indsize) {
  * Performs one generation of evolution on a given population.
  */
 void evolvePopulation(
-        individual *population, int popsize, int indsize, 
+        individual *population, individual *nextpop, int popsize, int indsize, 
         double swap_mutation_rate, double inversion_mutation_rate,
-        int elitesize
+        int elitesize,
+        int tournament_size, double crossover_rate
     ) {
 
-    // Mutate the population
-    for (int i = elitesize; i < popsize; i++) {
-        
-        if ((rand() % 10000)/10000.0 <= swap_mutation_rate) {
-            swapMutation(&(population[i]), indsize);
-        }
+    // Copy the elite from population[0 : elitesize] to nextpop[0 : elitesize]
+    for (int i = 0; i < elitesize; i++) {
+        nextpop[i].fitness = population[i].fitness;
+        for (int j = 0; j < indsize; j++)
+            nextpop[i].perm[j] = population[i].perm[j];
+    }
 
-        if ((rand() % 10000)/10000.0 <= inversion_mutation_rate) {
-            subsequenceInversionMutation(&(population[i]), indsize);
+    // Combine the population via sex (parent selection and crossover)
+    for (int i = elitesize; i < popsize; i += 2) {
+        couple parents = tournament(tournament_size, population, popsize);
+
+        if ((rand() % 10000) / 10000.0 <= crossover_rate) {
+            // Recombine the parents with a cross over strategy
+            if (i + 1 < popsize) {
+                crossOver(parents, &(nextpop[i]), &(nextpop[i + 1]), indsize);
+            } else {
+                crossOver(parents, &(nextpop[i]), NULL, indsize);
+            }
+
+        }else{
+            // Copy the parents "as is"
+            nextpop[i].fitness = parents.a->fitness;
+            for (int j = 0; j < indsize; j++)
+                nextpop[i].perm[j] = parents.a->perm[j];
+
+            if (i + 1 < popsize) {
+                nextpop[i+1].fitness = parents.a->fitness;
+                for (int j = 0; j < indsize; j++)
+                    nextpop[i+1].perm[j] = parents.a->perm[j];
+            }
         }
     }
 
-    evalPopFitness(population, popsize, indsize);
+    // Mutate the next population
+    for (int i = elitesize; i < popsize; i++) {
+        
+        if ((rand() % 10000)/10000.0 <= swap_mutation_rate) {
+            swapMutation(&(nextpop[i]), indsize);
+        }
 
-    // Sort individuals by fitness
-    qsort(population, popsize, sizeof(individual), fit_cmp);
+        if ((rand() % 10000)/10000.0 <= inversion_mutation_rate) {
+            subsequenceInversionMutation(&(nextpop[i]), indsize);
+        }
+    }
+
+    // Re-evaluate the population's fitness and sort the individuals by fitness
+    evalPopFitness(nextpop, popsize, indsize);
+    qsort(nextpop, popsize, sizeof(individual), fit_cmp);
+}
+
+/**
+ * Choose a couple of parents by k-way tournament selection.
+ */
+couple tournament(int tsize, individual *population, int popsize) {
+
+    int used[tsize];
+    memset(used, -1, sizeof(int) * tsize);
+
+    individual *individuals[tsize];
+
+    for (int i = 0; i < tsize; i++) {
+        int idx = rand() % popsize;
+
+        // Re-assign a random value untill its unique in this tournament
+        for (int j = 0; j < tsize; j++) {
+            if (used[j] == idx) {
+                idx = rand() % popsize;
+                j = 0;
+            }
+        }
+        individuals[i] = &(population[idx]);
+    }
+
+    qsort(individuals, tsize, sizeof(individual *), fit_cmp_ptr);
+
+    couple parents;
+    parents.a = individuals[0];
+    parents.b = individuals[1];
+
+    return parents;
+}
+
+/**
+ * Creates two new individuals based on the recombination of two parent individuals.
+ * 
+ * The crossover algorithm used is based on combining portions of each parent that have
+ * references to the exact same positions. 
+ * 
+ * For example, for parents:
+ * a = [5, 3, 2, 4, 1, 0]
+ * b = [1, 2, 0, 5, 4, 3]
+ * 
+ * The following portions will be combined:
+ * [5, -, -, 4, 1, -]
+ * [-, 2, 0, -, -, 3]
+ * and
+ * [-, 3, 2, -, -, 0]
+ * [1, -, -, 5, 4, -]
+ */
+void crossOver(couple parents, individual *maria, individual *zezinho, int indsize) {
+
+    char used[indsize];
+    memset(used, 0, sizeof(char) * indsize);
+
+    int mothermap[indsize];
+
+    for (int i = 0; i < indsize; i++) {
+        mothermap[parents.b->perm[i]] = i;
+    }
+
+    individual *father = parents.a;
+    individual *mother = parents.b;
+
+    int count = 0;
+    int idx = 0;
+    while (count < indsize ) {
+
+        for (int i = 0; i < indsize; i++) {
+            if (!used[i]) {
+                idx = i;
+                break;
+            }
+        }
+
+        // Decide whether each son will receive from one parent or the other
+        double r = (rand() % 10000) / 10000.0;
+        
+        while (!used[idx]) {
+            if (r < 0.5) {
+                if (maria) maria->perm[idx] = mother->perm[idx];
+                if (zezinho) zezinho->perm[idx] = father->perm[idx];
+            } else {
+                if (maria) maria->perm[idx] = father->perm[idx];
+                if (zezinho) zezinho->perm[idx] = mother->perm[idx];
+            }
+
+            count++;
+            used[idx] = 1;
+            idx = mothermap[father->perm[idx]];
+        }
+    }
 }
 
 /**
@@ -293,6 +460,21 @@ void subsequenceInversionMutation(individual *ind, int indsize) {
 int fit_cmp(const void *ls, const void *rs) {
     individual *a = (individual *)ls;
     individual *b = (individual *)rs;
+
+    if (a->fitness < b->fitness)
+        return -1;
+    else if (a->fitness > b->fitness)
+        return 1;
+    else
+        return 0;
+}
+
+/**
+ * Compares the fitness of two pointers to individuals.
+ */
+int fit_cmp_ptr(const void *ls, const void *rs) {
+    individual *a = *(individual **)ls;
+    individual *b = *(individual **)rs;
 
     if (a->fitness < b->fitness)
         return -1;
